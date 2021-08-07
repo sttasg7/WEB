289add5778722af8f48317220d40c7debd6f46fa
Kapnisis <kapnisis@ceid.upatras.gr>
2021-08-07 19:09:42 +0300

    admin graphs/tables v0.2
    so far away still

--- frontend/admin.php
+++ frontend/admin.php
@@ -11,46 +11,106 @@ include '../backend/logincheck.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="../css/mine.css" rel="stylesheet">
     <link href="../css/login.css" rel="stylesheet">
-    <link rel="icon" href="https://i.imgur.com/qY7kRzP.png" type="img/png">
+    <link rel="icon" href="https://i.imgur.com/qY7kRzP.png" type="img/png">    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.0/chart.min.js"></script>
     <script src="../js/palette.js"></script>
+    <script src="../js/admin.js"></script>
 
 </head>
 <style>
-    
+    .sb-main {
+        padding: 5%;
+        color: white;
+        background: grey;
+    }
+
+    .sb-sub {
+        margin-left: 40%
+    }
+
+    .bb {
+        background: lightgrey;
+    }
 </style>
 <body>
-
-
-<div class="container">
-    <div>
-        <canvas id="ch1" width="40%" height="10px"></canvas>
-        <canvas id="ch2" width="40%" height="10px"></canvas>
-
- 
-    <div class="col-8 pt-4" id="stats"># of users registered: <b> <span id="usercount"></span></b></div>
-
- 
-</div>
-
-
-
- <footer class="footer">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container">
-            <div class="copyright float-left">
-                <p class="mt-5 mb-3 text-muted"> Made by CR7-SKE &copy;2021</p>
+        <?php 
+        if ($loginst == 1){ ?>
+            <a href="../index.html">
+                <img src="../pictures/har.png" height="50px" width="auto" alt="HAR Observation & Statistics">
+            </a>  
+        <?php } else { ?>
+            <a href="../frontend/user-profile.php">
+                <img src="../pictures/har.png" height="50px" width="auto" alt="HAR Observation & Statistics">
+            </a>  
+        <?php } ?> 
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="../frontend/user-profile.php">Home</a>
+                    </li>
+                    <li lass="nav-item">
+                        <a class="nav-link" href="../frontend/user-upload.php">Upload</a>
+                    </li>
+                    <li lass="nav-item">
+                        <a class="nav-link" href="../frontend/user-map.html">Map</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="../frontend/faq.php">FAQs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../frontend/about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="d-grid gap-2 d-md-block d-none d-md-block" style="margin-right: -5%">
+                <form action="../backend/logout.php" method="post">
+                    <input type="submit" class="btn btn-danger" name="logout" value="Log Out"></input>
+                </form>
             </div>
         </div>
-    </footer>
+    </nav>
 
+<div class="container py-3">
+    <section style="display: flex; flex-direction: row; ">
+        <div style="width:20%;">
+        <ul class="navbar-nav bb">            
+            <li class="sb-main">Methods</li>
+            <li class="sb-sub"><input type="button" name="save" class="btn-sm btn-outline-secondary" value="Table" id="methodstable"></li>
+            <li class="sb-sub"><input type="button" name="save" class="btn-sm btn-outline-secondary" value="Graph" id="methods"></li>
+            <li class="sb-main">Status</li>
+            <li class="sb-sub"><input type="button" name="save" class="btn-sm btn-outline-secondary" value="Table" id="statusstable"></li>
+            <li class="sb-sub"><input type="button" name="save" class="btn-sm btn-outline-secondary" value="Graph" id="status"></li>
+            <li class="sb-main"><input type="button" name="save" class="btn btn-primary" value="Graph" id="graph"></li>
+        </ul>
+        </div>
 
-    
+        <div id="xanax" style="width:60%; margin-left: 2%; border-style: solid;">
+        <canvas id="ch1"></canvas>                       
+        </div>
+        <div id="table" style="width:60%; margin-left: 2%; border-style: solid;" class="text-center"></div>
+
+        <div style="width:20%; margin-left:2%">
+        <ul id="filt" class="navbar-nav bb"></ul>
         
-    
+        </div>
+
+</section>
+
+
+</div>
 
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="../js/admin.js"></script>
+
 </body>
 </html>
--- js/admin.js
+++ js/admin.js
@@ -1,102 +1,169 @@
-$(document).ready(function() { 
-var data;
-    $.ajax({
-        type: "POST",
-        url: "../backend/get-admin.php",
+var json;
+var check = 0;
+$.ajax({
+    type: "POST",
+    url: "../backend/get-admin.php",
+    data: {
+      type: 1
+    },
+    cache: false,
+    success: function(response){
+      json = response;
+    }
+  });
+
+$(document).ready(function(){
+    $("#xanax").hide();
+    $("#table").hide();
+
+    let myChart = document.getElementById('ch1').getContext('2d');
+    let chart = new Chart(myChart, {
+        type: 'pie',
         data: {
-            type: 1
+            labels: "",
+            datasets:[{
+                label: 'entries',
+                data: [],
+                borderWidth: 2,
+                borderColor: '#111',
+                backgroundColor: ""
+            }]
         },
-        cache: false,          
-        success: function(response){           
-          console.log(response);
-          showData(response);                
-        }
-      });
+        options: {
+            legend: {
+                "display": true
+              },
+              tooltips: {
+                "enabled": true
+              },  
+              plugins: {
+                  title: {
+                    display: true,
+                    text: ""
+                  }
+              }
+          }
+    });
+    
+    var type = 0;    
+    //updateChart(type, chart);
+    $("#methodstable").on('click', function(){
+      type = 0;
+      showTable(type);
+    })
+
+    $("#statusstable").on('click', function(){
+      type = 1;
+      showTable(type);
+    })
+    
+    $("#methods").on('click', function(){
+        type = 0;
+        updateChart(type, chart);
+    });
+
+    $("#status").on('click', function(){
+        type = 1;
+        updateChart(type, chart);
+    });
+
+    $("#graph").on('click', function(){
+        type = 2;
+        updateChart(type, chart);
+    });
 
 });
 
-function showData(r) {
-    
-    var f = JSON.parse(r);
 
+function updateChart(type, chart) {  
+    $("#table").hide();
+    var f = JSON.parse(json);
+    var l,d,txt;
+    if(type==0){
+        l = f.methods.map(function(e) {return e.name;});
+        d = f.methods.map(function(e) {return e.count;});
+        txt = "Methods";
+        //oops wrong question addfilters(type, l, d);
+      } else if(type==1) {
+        l = f.status.map(function(e) {return e.name;});
+        d = f.status.map(function(e) {return e.count;});
+        txt = "Response Codes";
+        //addfilters(type, l, d);
+      } else {
+        l = ["a","b"];
+        d = ["8","2"];
+      }
     
-    var labels = f.methods.map(function(e) {
-        return e.name;
-     });
-     var data = f.methods.map(function(e) {
-        return e.count;
-     });
-
-     console.log(labels.length);
-
-    
-    let myChart = document.getElementById('ch1').getContext('2d');
-    let massPopChart = new Chart(myChart, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets:[{
-                    label: 'entries',
-                    data: data,
-                    borderWidth: 2,
-                    borderColor: '#111',
-                    backgroundColor: palette('tol', data.length).map(function(hex) {
-                        return '#' + hex;
-                      })
-                }]
-            },
-            options: {
-                legend: {
-                    "display": true
-                  },
-                  tooltips: {
-                    "enabled": true
-                  },
-                indexAxis: 'y'
-            }
-        })
-
-///
-
-        var labels2 = f.status.map(function(e) {
-            return e.name;
-         });
-         var data2 = f.status.map(function(e) {
-            return e.count;
-         });
-    
-         console.log(labels.length);
-    
-    
-   
-        let vv = document.getElementById('ch2').getContext('2d');
-        let aa = new Chart(vv, {
-                type: 'bar',
-                data: {
-                    labels: labels2,
-                    datasets:[{
-                        data: data2,
-                        borderWidth: 2,
-                        borderColor: '#111',
-                        backgroundColor: palette('tol', data.length).map(function(hex) {
-                            return '#' + hex;
-                          })
-                    }]
-                },
-                options: {
-                    legend: {
-                        "display": true
-                      },
-                      tooltips: {
-                        "enabled": true
-                      },
-                    indexAxis: 'y'
-                }
-            })
-    
-};
+    const c = palette('tol', d.length).map(function(hex) {return '#' + hex;});
+    chart.data.datasets[0].data = d;
+    chart.data.labels = l;
+    chart.data.datasets[0].backgroundColor = c;
+    chart.options.plugins.title.text = txt;
+    chart.update();
+    $("#xanax").show(); 
+}
 
 
-//function updateChart() {
-  //  const filterData = myChart.data.datasets[0].data.filterData(value => )
-//}
+function showTable(type) {
+  $("#table").empty();
+  $("#xanax").hide();
+  $("#table").show();
+  var f = JSON.parse(json);
+  var x ="";
+  if(type==0){
+    l = f.methods.map(function(e) {return e.name;});
+    d = f.methods.map(function(e) {return e.count;});
+    txt = "Methods";
+    //oops wrong question addfilters(type, l, d);
+  } else if(type==1) {
+    l = f.status.map(function(e) {return e.name;});
+    d = f.status.map(function(e) {return e.count;});
+    txt = "Response Codes";
+    //addfilters(type, l, d);
+  } else {
+    l = ["a","b"];
+    d = ["8","2"];
+  }
+
+  x += "<table class='table table-bordered'><caption>"+txt+"</caption><table>";
+  for(let i=0; i<l.length; i++) {
+    x += "<tr><th scope='row'>"+l[i]+"</th><td>"+d[i]+"</td></tr>";
+  }
+  x +="</tbody>";
+
+  $("#table").append(x);
+}
+
+/*
+function addfilters(type, str, dt) {
+  //empty filter div
+  $("#filt").empty();
+  i = 0;
+  //populate filter div with the amount of labels from str
+  let x ="<h6>filters</h6>";
+  str.forEach(str => {
+    x += '<li"><input type="checkbox" checked value="'+ str +'" id="f' +i+ '""><label for="'+str+'">'+str+'</label></li><br>';
+    i++;
+  });
+  x += '<form><input type="button" class="btn btn-info" id="filterbtn" name="filter" value="Filter"></input></form>'; //filter button
+  $("#filt").append(x);
+
+  //add action to filter button
+  $("#filterbtn").on('click', function(){
+    //get array with checkbox values
+    var z = [];
+    for(let j=0; j<i; j++) {
+      if($("#f"+j).is(":checked")) {z[j]="1";} 
+      else {z[j]="0";}
+        }
+      console.log(z);
+
+    //call filter function
+    filterchart(z, str, dt);
+    });
+}
+
+function filterchart(boxes, lbls, data){
+
+}
+*/
